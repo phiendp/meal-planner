fd6f97a5b5382609c4e507d046c1fbf1cc3c41bf
Phien Pham <phien@triip.me>
2017-10-13 23:09:10 +0700

    Use connect() to curry and return a function that takes in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions'
 
 
 class App extends Component {
-
   render() {
     return (
       <div>
@@ -12,4 +13,31 @@ class App extends Component {
   }
 }
 
-export default App;
+
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meal: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+        return meals
+      }, {})
+    }))
+
+  }
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
